5f43ec4e0aefc5082b233269c3310fabd52d2593
dakatk <dakatk97@gmail.com>
2019-02-19 12:22:36 -0600

    Added URLs for images

--- Django/radar/views.py
+++ Django/radar/views.py
@@ -2,23 +2,38 @@ from django.shortcuts import render
 from django.http import Http404, JsonResponse
 from django.views.decorators.csrf import csrf_protect
 
-# Number of images to loop through (constant value)
-NUM_IMAGES = 23
+"""
+Number of images to loop through (constant value)
+"""
+NUM_IMAGES = 30
 
-# Parses values into regex to return a well-formatted
-# file path for a static image resource
-def image_file(type_id, index):
-    return 'images/radar/%s/%s_img%i.png' % (type_id, type_id, index)
+"""
+Base url for pulling images
+"""
+IMAGE_URL = 'http://bergeron.valpo.edu/~wxweb/radar/'
 
-# Parses the GET request that loads the radar page
+"""
+Returns the absolute url of an image file given type and index
+"""
+def image_file(radar_type, image_path, index):
+    global IMAGE_URL
+    return IMAGE_URL + image_path + f"/{radar_type}_img{index}.png"
+
+"""
+Parses the GET request that loads the radar page
+"""
 def radar(request):
     global NUM_IMAGES
 
     # Dictionary of allowed type ids and
     # their corresponding display names
-    radar_types = {'refl': 'Reflectivity',
+    radar_types = {'ref': 'Reflectivity',
                    'vel': 'Radial Velocity',
                    'zdr': 'Diff. Reflectivity'}
+
+    radar_path_names = {'ref': 'reflectivity',
+                        'vel': 'velocity',
+                        'zdr': 'diff_refl'}
 
     # Context data to help the page display some
     # data computed from the backend
@@ -32,6 +47,7 @@ def radar(request):
         if not (radar_type in radar_types):
             raise Http404('Page not found')
         else:
+            context['radar_path_name'] = radar_path_names[radar_type]
             context['radar_name'] = radar_types[radar_type]
             context['radar_type'] = radar_type
     else:
@@ -40,8 +56,11 @@ def radar(request):
     # HTML files are found in the site's 'templates' folder
     return render(request, 'radar.html', context)
 
-# Parses the POST request that the page uses to
-# retrieve specified data via JS and Ajax
+
+"""
+Parses the POST request that the page uses to retrieve
+specified data via JS and Ajax
+"""
 @csrf_protect
 def image_desc(request):
     global NUM_IMAGES
@@ -52,10 +71,11 @@ def image_desc(request):
         return HttpResonseForbidden()
 
     radar_type = request.POST['radar_type']
+    image_url = request.POST['image_path']
 
     # List of every image path used in the loop
     # (setup in the backend helps with this a lot)
-    image_path = lambda i: image_file(radar_type, i + 1)
+    image_path = lambda i: image_file(radar_type, image_url, i)
     image_cycle = list(map(image_path, range(NUM_IMAGES)))
     
     context = {'image_cycle': image_cycle}
--- Django/static/js/radar.js
+++ Django/static/js/radar.js
@@ -1,32 +1,51 @@
 $(function () {
 
   const csrf_token = $('[name=csrfmiddlewaretoken]').val();
-  const static_url = $('[name=static_url]').val();
   const radar_type = $('[name=radar_type]').val();
+  const image_path = $('[name=image_path]').val();
 
-  const loop_delay = 50;
-  const loop_fade = 500;
+  const loop_delay = 0;
+  const loop_fade = 1000;
   
   var image_cycles = [''];
   var image_index = 0;
 
+  /*
   var tag_first = 0;
   var tag_next = 1;
-  
+  */
+
   function loop_images() {
 
-    var next_index = image_index + 1;
+    let next_index = image_index + 1;
+    if (next_index >= image_cycles.length) {
+      next_index = 0;
+    }
+
+    let image_tags = $('#fade_loop img');
+
+    image_tags.eq(image_index).fadeOut(loop_fade).end()
+		.eq(next_index).hide().fadeIn(loop_fade, loop_images);
+
+    
+    image_index ++;
+    if (image_index >= image_cycles.length) {
+      image_index = 0;
+    }
+
+    /*
+    let next_index = image_index + 1;
     if (next_index >= image_cycles.length) {
       next_index = 0;
     }
 
     var image_tags = $('#fade_loop img');
 
-    image_tags.eq(tag_first).attr('src', static_url + image_cycles[image_index]);
-    image_tags.eq(tag_next).attr('src', static_url + image_cycles[next_index]);
+    image_tags.eq(tag_first).attr('src', image_cycles[image_index]);
+    image_tags.eq(tag_next).attr('src', image_cycles[next_index]);
 
-    image_tags.eq(tag_first).delay(loop_delay).fadeOut(loop_fade).end()
-		.eq(tag_next).delay(loop_delay).hide().fadeIn(loop_fade, loop_images);
+    image_tags.eq(tag_first).fadeOut(loop_fade).end()
+		.eq(tag_next).hide().fadeIn(loop_fade, loop_images);
 
     tag_first = 1 - tag_first;
     tag_next = 1 - tag_next;
@@ -35,14 +54,7 @@ $(function () {
     if (image_index >= image_cycles.length) {
       image_index = 0;
     }
-  
-    /*
-    // lightwieght image loop:
-
-    window.setTimeout(
-      function () {
-        loop_images();
-      }, loop_delay);*/
+    */
   }
 
   $.ajax({
@@ -50,7 +62,8 @@ $(function () {
     url: 'image_desc/',
     dataType: 'JSON',
     data: {
-      'radar_type': radar_type
+      'radar_type': radar_type,
+      'image_path': image_path
     },
     headers: {
       'X-CSRFTOKEN': csrf_token
@@ -58,6 +71,13 @@ $(function () {
     success: function(response) {
 
       image_cycles = response['image_cycle'];
+      for (i in image_cycles) {
+        let tag = '<img ';
+        if (i !== 0) {
+          tag += 'style="display: none;"';
+        }
+	$('#fade_loop').append(tag + ' src="' + image_cycles[i] + '" />');
+      }
       loop_images();
     }
   });
