91f5522253cb8f929a9ef09984dd4bd7a3a94fb5
dakatk <dakatk97@gmail.com>
2019-02-20 16:35:37 -0600

    Made image loops presentable, fixed unit tests

--- Django/radar/tests.py
+++ Django/radar/tests.py
@@ -1,13 +1,14 @@
 from django.test import TestCase, Client
 from .views import NUM_IMAGES
 
+
 class RadarTestCase(TestCase):
     def setUp(self):
         self.num_images = NUM_IMAGES
         self.client = Client()
 
-    def test_post_refl_images(self):
-        response = self.client.post('/radar/image_desc/', {'radar_type': 'refl'})
+    def test_post_ref_images(self):
+        response = self.client.post('/radar/image_desc/', {'radar_type': 'ref'})
         image_cycle = response.json()['image_cycle']
         
         self.assertEqual(type(image_cycle), list)
--- Django/static/js/radar.js
+++ Django/static/js/radar.js
@@ -2,59 +2,25 @@ $(function () {
 
   const csrf_token = $('[name=csrfmiddlewaretoken]').val();
   const radar_type = $('[name=radar_type]').val();
-  const image_path = $('[name=image_path]').val();
 
-  const loop_delay = 0;
-  const loop_fade = 1000;
+  const loop_delay = 100;
   
   var image_cycles = [''];
   var image_index = 0;
 
-  /*
-  var tag_first = 0;
-  var tag_next = 1;
-  */
-
   function loop_images() {
-
-    let next_index = image_index + 1;
-    if (next_index >= image_cycles.length) {
-      next_index = 0;
-    }
-
-    let image_tags = $('#fade_loop img');
-
-    image_tags.eq(image_index).fadeOut(loop_fade).end()
-		.eq(next_index).hide().fadeIn(loop_fade, loop_images);
-
     
     image_index ++;
     if (image_index >= image_cycles.length) {
       image_index = 0;
     }
 
-    /*
-    let next_index = image_index + 1;
-    if (next_index >= image_cycles.length) {
-      next_index = 0;
-    }
+    let image_tag = $('#fade_loop #current_image');
+    image_tag.attr('src', image_cycles[image_index]);
 
-    var image_tags = $('#fade_loop img');
-
-    image_tags.eq(tag_first).attr('src', image_cycles[image_index]);
-    image_tags.eq(tag_next).attr('src', image_cycles[next_index]);
-
-    image_tags.eq(tag_first).fadeOut(loop_fade).end()
-		.eq(tag_next).hide().fadeIn(loop_fade, loop_images);
-
-    tag_first = 1 - tag_first;
-    tag_next = 1 - tag_next;
-  
-    image_index ++;
-    if (image_index >= image_cycles.length) {
-      image_index = 0;
-    }
-    */
+    window.setTimeout(function () {
+        loop_images();
+    }, loop_delay);
   }
 
   $.ajax({
@@ -62,8 +28,7 @@ $(function () {
     url: 'image_desc/',
     dataType: 'JSON',
     data: {
-      'radar_type': radar_type,
-      'image_path': image_path
+      'radar_type': radar_type
     },
     headers: {
       'X-CSRFTOKEN': csrf_token
@@ -71,13 +36,6 @@ $(function () {
     success: function(response) {
 
       image_cycles = response['image_cycle'];
-      for (i in image_cycles) {
-        let tag = '<img ';
-        if (i !== 0) {
-          tag += 'style="display: none;"';
-        }
-	$('#fade_loop').append(tag + ' src="' + image_cycles[i] + '" />');
-      }
       loop_images();
     }
   });
