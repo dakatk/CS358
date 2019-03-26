102efe5593787f9452073195f5596f8385197db3
electrolitic <electrolitic21@gmail.com>
2019-03-26 12:47:01 -0500

    Created the Station Climo page

---
+++ static/js/station_climo.js
@@ -0,0 +1,24 @@
+$(function() {
+
+    const current_station = $('[name=current_station]').val();
+    const csrf_token = $('[name=csrfmiddlewaretoken]').val();
+
+    $.ajax({
+        type: 'POST',
+        url: 'file_names/',
+        dataType: 'json',
+        data: {'current_station': current_station},
+        headers: {
+          'X-CSRFTOKEN': csrf_token
+        },
+        success: function(response) {
+            let files = response['file_names'];
+            for(let i = 0; i < files.length; i++) {
+                $("#station").append('<option value="' + files[i] + (files[i] === current_station ? '" selected>' : '">') + files[i] + "</option>");
+            }
+            let historyData = response['history_data'];
+            //alert(historyData);
+            $("#station_history").append(historyData);
+        }
+    });
+});
