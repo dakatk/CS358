e229d4427291ee9fc55da3c9166745ed7178ab3a
dakatk <dakatk97@gmail.com>
2019-03-28 16:25:37 -0500

    Added all UI setup in JS

---
+++ soundings/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+# Specifies URL paths within this app
+# (relative to the app's base URL, not the site's)
+urlpatterns = [
+
+    path('', views.soundings, name='soundings'),
+    path('images/', views.images, name='images')
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
