ebb6fa4361e35840f9910eb7804df46db91efa16
Semicolon <dakatk97@gmail.com>
2019-03-29 13:33:31 -0500

    Fixed satellite.js

--- static/js/satellite.js
+++ static/js/satellite.js
@@ -1,3 +1,20 @@
-$(function () {  const satellite_channel = $('[name=satellite_channel]').val();  require.config({    baseUrl: '/static/js/modules',    paths: {      'loop': 'loop'    }  });  require([    'loop'  ], function (module) {
-    module.request_image_loop('images/', { 'channel': satellite_channel }, 200);
-    module.ui_setup();  });});
+$(function () {
+  
+  const satellite_channel = $('[name=satellite_channel]').val();
+  
+  require.config({
+    
+    baseUrl: '/static/js/modules',
+    paths: {
+      'loop': 'loop'
+    }
+  });
+  
+  require([
+    'loop'
+  ], function (module) {
+    
+    module.request_image_loop('images/', { 'channel': satellite_channel }, 5);
+    module.ui_setup();
+  });
+});
