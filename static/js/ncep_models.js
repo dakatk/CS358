9aa42cde16f9b0b1ad9071be00154d39bc0df9bf
Semicolon <dakatk97@gmail.com>
2019-03-29 13:31:01 -0500

    Fixed ncep_models.js

--- static/js/ncep_models.js
+++ static/js/ncep_models.js
@@ -1,3 +1,18 @@
-$(function () {  require.config({    baseUrl: '/static/js/modules',    paths: {      'loop': 'loop'    }  });  require([    'loop'  ], function (module) {
-    module.request_image_loop('images/', {}, 200);
-    module.ui_setup();  });});
+$(function () {
+  
+  require.config({
+    
+    baseUrl: '/static/js/modules',
+    paths: {
+      'loop': 'loop'
+    }
+  });
+  
+  require([
+    'loop'
+  ], function (module) {
+    
+    module.request_image_loop('images/', {}, 5);
+    module.ui_setup();
+  });
+});
