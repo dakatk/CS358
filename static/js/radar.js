21dcdc954b50b060a569ee6468ac4cab443bd18c
Semicolon <dakatk97@gmail.com>
2019-03-29 13:32:14 -0500

    Fixed radar.js

--- static/js/radar.js
+++ static/js/radar.js
@@ -1,1 +1,20 @@
-$(function () {  const radar_type = $('[name=radar_type]').val();  require.config({    baseUrl: '/static/js/modules',    paths: {      'loop': 'loop'    }  });  require([    'loop'  ], function (module) {    module.request_image_loop('images/', { 'radar_type': radar_type }, 200);    module.ui_setup();  });});
+$(function () {
+  
+  const radar_type = $('[name=radar_type]').val();
+  
+  require.config({
+    
+    baseUrl: '/static/js/modules',
+    paths: {
+      'loop': 'loop'
+    }
+  });
+  
+  require([
+    'loop'
+  ], function (module) {
+    
+    module.request_image_loop('images/', { 'radar_type': radar_type }, 5);
+    module.ui_setup();
+  });
+});
